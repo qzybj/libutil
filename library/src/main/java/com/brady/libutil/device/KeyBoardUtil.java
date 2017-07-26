ceea42628285dc61feefb3aebc9a67660c791e7d
Lucky <Clair@Lucky.local>
2017-07-26 15:03:38 +0800

    1.添加键盘显示隐藏方法

--- library/src/main/java/com/brady/libutil/device/KeyBoardUtil.java
+++ library/src/main/java/com/brady/libutil/device/KeyBoardUtil.java
@@ -1,14 +1,79 @@
 package com.brady.libutil.device;
 
+import android.app.Activity;
+import android.content.Context;
+import android.os.Handler;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.brady.libutil.log.CLog;
 
 /**
  * 键盘快捷键 操作
  */
 public class KeyBoardUtil {
+
+    /**
+     * 显示软键盘
+     * @param view
+     */
+    public static void showInputMethod(View view) {
+        if(view!=null){
+            InputMethodManager imm =
+                    (InputMethodManager) view.getContext()
+                            .getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (imm != null) {
+                imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT);
+            }
+        }
+    }
+
+    /**
+     * 显示软键盘
+     * @param context
+     */
+    public static void showInputMethod(Context context) {
+        if(context!=null){
+            InputMethodManager imm =
+                    (InputMethodManager) context
+                            .getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
+
+    /**
+     * 强制隐藏软键盘
+     * @param activity
+     */
+    public static void hideSoftInput(Activity activity) {
+        if (activity != null&&!activity.isFinishing()) {
+            try {
+                View v = activity.getWindow().peekDecorView();
+                if (v != null && v.getWindowToken() != null) {
+                    InputMethodManager imm = (InputMethodManager) activity
+                            .getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            } catch (Exception e) {
+                CLog.e(e.toString());
+            }
+        }
+    }
+    /** 多少时间后显示软键盘 */
+    public static void showInputMethod(final View view, long delayMillis) {
+        // 显示输入法
+        new Handler().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        showInputMethod(view);
+                    }
+                }, delayMillis);
+    }
 
     /**
      * 显示完成
@@ -79,7 +144,6 @@ public class KeyBoardUtil {
                 }
             });
         }
-
     }
 
     public interface ShortcutKeyCallback{
